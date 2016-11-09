83629e57b40de44b44ccf82497f06c4fda9d3a87
dclaudel <damienclaudel88@gmail.com>
2016-11-09 16:32:26 +0100

    recursivite completer

--- TP0/CppApplication_2/recursivite.cpp
+++ TP0/CppApplication_2/recursivite.cpp
@@ -12,14 +12,65 @@
  */
 
 #include <cstdlib>
+#include <iostream>
 
 using namespace std;
 
 /*
  * 
+ * 
+ * 
  */
+
+int factorielleIter(int n)
+// {n ≥ 0} => {résultat = n!(soit 1×2×...×n−1×n}
+{
+    int somme=1;
+    while(n>0){
+        somme=somme*n;
+        n--;
+    }
+    return somme;
+}
+
+int factorielleRec(int n)
+// {n ≥ 0} => {résultat = n!(soit 1×2×...×n−1×n}
+{
+    if(n<2){
+        return 1;
+    }
+    else{
+        return n*factorielleRec(n-1);
+    }
+}
+
+bool estPair(int n){
+    if(n==0){
+        return true;
+    }
+    else if(n==1){
+        return false;
+    }
+    else{
+        return estPair(n-2);
+    }
+}
+
+
+
+
+
 int main(int argc, char** argv) {
 
+    int n=6;
+    int somme=factorielleIter(n);
+    cout<<somme<<endl;
+    int somme2=factorielleRec(n);
+    cout<<somme2<<endl;
+    if(estPair(n))
+        cout<<"n est pair"<<endl;
+    else
+        cout<<"n est impair"<<endl;
     return 0;
 }
 
